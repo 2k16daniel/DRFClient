7319d044831a11c44a783384c10907166648305d
2k16daniel <2k16daniel@gmail.com>
2021-11-15 19:15:06 +0800

    needed external dependency is ready

---
+++ lib/src/main/java/github/_2k16daniel/drfclient/DRFClientBuilder.java
@@ -0,0 +1,13 @@
+package github._2k16daniel.drfclient;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.apache.http.HttpHost;
+
+public class DRFClientBuilder {
+    
+}
---
+++ lib/src/main/java/github/_2k16daniel/drfclient/utils/Utils.java
@@ -0,0 +1,5 @@
+package github._2k16daniel.drfclient.utils;
+
+public class Utils {
+    
+}
