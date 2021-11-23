24129bf3b68a5230f4df66338c1ca401d5917be2
2k16daniel <2k16daniel@gmail.com>
2021-11-23 21:50:49 +0800

    implemented builder class for DRFClient

--- lib/src/main/java/github/_2k16daniel/drfclient/DRFClientBuilder.java
+++ lib/src/main/java/github/_2k16daniel/drfclient/DRFClientBuilder.java
@@ -1,13 +1,65 @@
 package github._2k16daniel.drfclient;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 
-import org.apache.http.HttpHost;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.lang.reflect.Constructor;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 public class DRFClientBuilder {
+
+    public HttpClient client;
+    public ObjectMapper mapper;
+    public DRFRequestInterceptor intercept;
+    public Class<? extends DRFClient> DRFClass;
     
+    DRFClientBuilder(){ /* empty constructor */ }
+
+    public DRFClientBuilder httpclient(HttpClient myclient){
+        this.client = myclient;
+        return this;
+    }
+
+    public DRFClientBuilder objMapper(ObjectMapper mapper){
+        this.mapper = mapper;
+        return this;
+    }
+
+    public DRFClientBuilder requestInterceptor (DRFRequestInterceptor intercept){
+        this.intercept = intercept;
+        return this;
+    }
+
+    public DRFClientBuilder DRFClientCLass(Class<? extends DRFClient> DRFClass){
+        this.DRFClass = DRFClass;
+        return this;
+    }
+
+    public DRFClient builder(){
+
+        if (DRFClass == null){
+            DRFClass = DRFClient.class;
+        }
+        if (mapper == null){
+            mapper = new ObjectMapper();
+            mapper.setSerializationInclusion(Include.NON_NULL);
+        }
+        if (client == null){
+            client = HttpClientBuilder.create().useSystemProperties().build();
+        }
+
+        return buildRestCLient(this, DRFClass);
+    }
+
+    private <T extends DRFClient> T buildRestCLient(DRFClientBuilder builder, Class<T> DRFClientCLass){
+        try{
+            Constructor<T> constructThis = DRFClientCLass.getDeclaredConstructor(DRFClientCLass);
+            constructThis.setAccessible(true);
+            return constructThis.newInstance(builder);
+        }
+        catch (Exception error){
+            throw new RuntimeException(error);
+        }
+    }
 }
---
+++ lib/src/main/java/github/_2k16daniel/drfclient/DRFRequestInterceptor.java
@@ -0,0 +1,10 @@
+package github._2k16daniel.drfclient;
+
+import org.apache.http.client.methods.HttpRequestBase;
+
+public class DRFRequestInterceptor {
+    
+    public void requestInterceptor(HttpRequestBase myrequest){
+
+    }
+}
